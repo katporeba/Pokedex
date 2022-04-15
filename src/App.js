37268738ec86466f8eb1ce5b5bb07ce103bdb36c
Katarzyna Poreba <katarzynaporeba@icloud.com>
2022-04-15 15:05:57 +0200

    Add basic fetch & components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import Cards from "./components/Cards";
 
-function App() {
+const App = () => {
+    const  [hasError, setErrors] =  useState(false);
+    const [pokemons, setPokemons] = useState([]);
+    const [nextPokemons, setNextPokemons] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+
+    const getData = async () => {
+        try {
+            const response = await fetch(nextPokemons);
+            const data = await response.json();
+            setNextPokemons(data.next);
+
+            for (const pokemon of data.results) {
+                const response = await fetch(pokemon.url);
+                const dataOfPokemon = await response.json();
+                setPokemons(pokemons => [...pokemons, dataOfPokemon]);
+            }
+        } catch (error) {
+            setErrors(error);
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className={"container"}>
+          <h1 className={"header"}>Pokedex</h1>
+          {!hasError && <Cards pokemons={pokemons}/>}
+          <button onClick={() => getData()}>Show more pokemons</button>
+      </div>
+  )
 }
-
 export default App;
